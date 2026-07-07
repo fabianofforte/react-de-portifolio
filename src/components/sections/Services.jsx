6955e76001fd3382128444747d94b0d14cec71cc
Fabiano Ferreira <fabiano.fforte@gmail.com>
2026-07-06 22:05:16 -0300

    commit services.jsx

---
+++ src/components/sections/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {services} from '../../data/services';
+import * as Icons from "lucide-react";
+import { Wrench } from 'lucide-react';
+import FadeIn from '../animations/FadeIn';
+
+{/* 1:38:02 */}
+
+function Services() {
+  return (
+    <div>Services</div>
+  )
+}
+
+export default Services
